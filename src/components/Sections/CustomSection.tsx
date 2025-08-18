03a796b811fa662d45db595a7276cb109149058a
Marcos Roberto <marcosprojetosdev@gmail.com>
2025-08-18 13:25:01 -0300

    feat: Update dependencies, add new images, and implement custom sections with parallax effect
    - Updated package.json to include new dependencies and adjust existing ones.
    - Added new images for chocolate cake and cupcake.
    - Created CustomSection component for reusable section layout.
    - Enhanced the main page with parallax effects using react-parallax-mouse.
    - Styled sections with new CSS for better visual presentation.

---
+++ src/components/Sections/CustomSection.tsx
@@ -0,0 +1,24 @@
+import colors from "tailwindcss/colors";
+interface CustomSectionProps {
+  bg?: string,
+  style?: React.CSSProperties;
+  children?: React.ReactNode;
+  className?: string;
+  ref?: React.Ref<HTMLDivElement>;
+}
+
+export function CustomSection({ bg = colors.white, style, children, className, ref }: CustomSectionProps){
+  return <>
+    <section
+      className={`w-full  text-purple-800 relative py-8 px-4 text-center md:text-start md:py-[80px] md:px-[126px]`}
+      style={{ backgroundColor: bg, ...style }}
+
+    >
+      {children && (
+        <div className={`w-full ${className}`} ref={ref}>  
+          {children}
+        </div>
+      )}
+    </section>
+  </>
+}
